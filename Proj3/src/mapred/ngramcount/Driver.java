9e76817badc21ec4c8f06aac94d48926452ee3b7
Jeffery <lqchen613@gmail.com>
2016-03-23 01:14:59 -0400

    finish the Ngram part

--- Proj3/src/mapred/ngramcount/Driver.java
+++ Proj3/src/mapred/ngramcount/Driver.java
@@ -8,20 +8,22 @@ import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 
 public class Driver {
-
 	public static void main(String args[]) throws Exception {
 		SimpleParser parser = new SimpleParser(args);
 
 		String input = parser.get("input");
 		String output = parser.get("output");
-
-		getJobFeatureVector(input, output);
+        // Get the n from the command line
+        String n = parser.get("n");
+		getJobFeatureVector(input, output, n);
 
 	}
 
-	private static void getJobFeatureVector(String input, String output)
+	private static void getJobFeatureVector(String input, String output, String n)
 			throws IOException, ClassNotFoundException, InterruptedException {
-		Optimizedjob job = new Optimizedjob(new Configuration(), input, output,
+        Configuration conf = new Configuration();
+        conf.set("N", n);
+		Optimizedjob job = new Optimizedjob(conf, input, output,
 				"Compute NGram Count");
 
 		job.setClasses(NgramCountMapper.class, NgramCountReducer.class, null);
--- Proj3/src/mapred/ngramcount/NgramCountMapper.java
+++ Proj3/src/mapred/ngramcount/NgramCountMapper.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import mapred.util.Tokenizer;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -14,11 +15,22 @@ public class NgramCountMapper extends Mapper<LongWritable, Text, Text, NullWrita
 	@Override
 	protected void map(LongWritable key, Text value, Context context)
 			throws IOException, InterruptedException {
-		String line = value.toString();
+		// Get the n from configuration
+        Configuration conf = context.getConfiguration();
+        final int n = Integer.parseInt(conf.get("N"));
+
+        String line = value.toString();
 		String[] words = Tokenizer.tokenize(line);
 
-		for (String word : words)
-			context.write(new Text(word), NullWritable.get());
-
+        int wordsLen = words.length;
+        for (int i = 0; i < wordsLen - n + 1; i++) {
+            // Get every n-grams
+            StringBuilder sb = new StringBuilder();
+            sb.append(words[i]);
+            for (int j = i+1; j < i + n; j++) {
+                sb.append(" " + words[j]);
+            }
+            context.write(new Text(sb.toString()), NullWritable.get());
+        }
 	}
 }
